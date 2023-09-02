d81dd8d25db6585bf34bed3e9a62ed89fdc09d14
kakaushow1 <kauani.santos@estudante.ifms.edu.br>
2023-09-02 16:08:36 -0400

    feito

---
+++ sw.js
@@ -0,0 +1,19 @@
+let cacheName = "Jogo_da_velha";
+let filesToCache = ["/", "/index.html",  "/css/style.css", "/js/main.js", "/img/icon-256.png"];
+
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
